c15de440b21e9504d03f9035a52c427c4870c973
SimonAllen <allen8101070@gmail.com>
2021-11-19 18:10:06 +0800

    Update link for video and watermark
    影片與圖片連結掛了，換一個連結

--- DEMO/main.js
+++ DEMO/main.js
@@ -1,6 +1,6 @@
 console.log("Hello")
 const player = videojs('my-video',{
-    sources:[{ src: "http://www.html5videoplayer.net/videos/toystory.mp4"}],
+    sources:[{ src: "https://www.w3schools.com/html/movie.mp4" }],
     loop:true,
     muted:true,
     width:"600",
@@ -9,7 +9,7 @@ const player = videojs('my-video',{
 });
 
 player.ITMAN_watermark({
-    file: 'http://literreviewstaffing.com/wp-content/uploads/2015/02/google-1088004_960_720-150x150.png',
+    file: "./G.png",
     position:"left-bottom",
     opacity: 0.3,
 });
